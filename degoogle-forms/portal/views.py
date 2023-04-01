14ba56fecbe46f4043d043072d30adc441f61ac5
Sourabh kumar verma <55890376+sourabhkv@users.noreply.github.com>
2023-04-01 19:40:42 +0530

    Database added

--- degoogle-forms/portal/views.py
+++ degoogle-forms/portal/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
-
+import csv
 # Create your views here.
 def signuppage(request):
     if request.method=='POST':
@@ -24,7 +24,13 @@ def signuppage(request):
 @login_required(login_url='login')
 def homepage(request):
     if request.method=='POST':
-        print(request.POST)
+        recieved_data =dict(request.POST)
+        with open('data.csv', mode='a') as file:
+            writer = csv.writer(file)
+            writer.writerow(
+                (recieved_data["csrfmiddlewaretoken"][0],request.user.username,recieved_data['name'][0],recieved_data['usn'][0],recieved_data['email'][0],
+                 recieved_data['semester'][0],recieved_data['branch'][0],recieved_data["Rating"][0],recieved_data["subject"][0],*recieved_data['programming-language'])
+            )
         return HttpResponse('Form submitted succesfully')
     print(request.user.username)
     return render(request,'deform.html')
